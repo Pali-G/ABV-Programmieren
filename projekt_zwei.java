2c09ee58f8bee9b71b85662fec406f350528e008
lia <64144611+lia129@users.noreply.github.com>
2020-04-30 14:13:33 +0200

    Add files via upload

---
+++ projekt_zwei.java
@@ -0,0 +1,51 @@
+package uebung;
+
+
+public class projekt_zwei {
+
+	public static void main(String[] args)
+	{
+	
+	//Attribute
+	  double x = 1.0;
+	  double y = 0.0;
+	  int m = 3;
+	  int n = 1;
+	  int max = 2147483647;
+	  int min = -2147483648;
+	  double z = Double.POSITIVE_INFINITY;
+
+	//Methoden, Ergebnisse ausgabe
+	 System.out.println("x/y = " + ( x/y));
+	
+	 System.out.println("-y/x = " + ( -y/x));
+	
+	 System.out.println("x/y + x/y = " + (x/y + x/y));
+	 
+	 System.out.println("x/y – x/y = " + (x/y - x/y));
+	 
+	 System.out.println("y/y = " + y/y);
+	 
+	 System.out.println("z/z = " + z/z);
+	 
+	 System.out.println("n/m = " + n/m);
+	 
+	 System.out.println("Math.sqrt (-x) = " + Math.sqrt (-x));
+	 
+	 System.out.println("Math.log (-x) = " + Math.log (-x));
+	 
+	 System.out.println("max + 1 = " + max+1);
+	 
+	 System.out.println("min - 1 = " + (min-1));
+	 
+	// System.out.println(Long.MAX VALUE);
+	 
+	// System.out.println(Double.MAX VALUE);
+	 
+	// System.out.println(Double.MAX VALUE*2);
+	 
+	 System.out.println("(max+1)== (min) = " + ((max+1)==(min)));
+	
+	}
+	
+}
