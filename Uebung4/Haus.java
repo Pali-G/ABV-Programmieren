c934597771b42612022d4fe48fa0861a912806da
antivampir <gerbip00@zedat.fu-berlin.de>
2020-05-15 18:15:07 +0200

    nicht funktionierender Konstruktor hinzugefügt

--- Uebung4/Haus.java
+++ Uebung4/Haus.java
@@ -1,6 +1,6 @@
 public class Haus{
     public static void main (String[] args){
-	int x = 5;
+	/*int x = 5;
 	boolean[] w = new boolean[5];
 	w[0] = true;
 	w[1] = false;
@@ -8,17 +8,37 @@ public class Haus{
 	w[3] = false;
 	w[4] = true;
 	boolean k = WohnungenBewohnt(w, x);
-	System.out.println(k);
+	System.out.println(k);*/
+	Haus Haus1 = new Haus(4);// 1, 1998, true
+	System.out.println(Haus1.WohnungenBewohnt());
+
+       
     }
-    int Anzahlwohnungen;
+    public int Anzahlwohnungen;
     int Hausnummer;//Grundstücknummer
     int Baujahr;
     boolean Hausleer;
     boolean gebaut;
     //evtl Strasse
-    boolean[] wohnungen = new boolean[Anzahlwohnungen-1];
     
-    public static boolean WohnungenBewohnt(boolean[] wohnungen, int Anzahlwohnungen){
+
+    public Haus(int AW){//, int Hausnummer, int Baujahr, boolean gebaut
+	this.Anzahlwohnungen = AW;
+	boolean[] wohnungen = new boolean[Anzahlwohnungen-1];
+	for (int i = 0; i < AW-1; i++){
+	    if (i%2==0){
+		wohnungen[i] = true;
+	    }else{
+		wohnungen[i] = false;
+	    }
+	}
+    }
+
+    
+
+    public boolean WohnungenBewohnt(){//boolean[] wohnungen, int Anzahlwohnungen
+	this.AW = Anzahlwohnungen;
+	this.wohnungen = wohnungen;
 	int bewohnt = 0;
 	boolean leer = false;
 	for (int i = 0; i <= wohnungen.length -1; i++){
@@ -32,6 +52,4 @@ public class Haus{
 	    }
 	return leer;
     }
-
-    
 }
