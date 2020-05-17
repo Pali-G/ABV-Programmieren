80b59c24ff7cf61ee8ab4f72abc00c33514c1547
antivampir <gerbip00@zedat.fu-berlin.de>
2020-05-17 16:46:50 +0200

    Ordner gelöscht

--- Uebung1/helloworld.java
+++
@@ -1,9 +0,0 @@
-//Lia Melissa Lochner, Jakob Degmair, Patricia Gerbig, Noah Frick
-
-public class helloworld{ //Neue Klasse wird erstellt.
-    public static void main (String[] args){//Main-Methode wird erstellt und auf "public" gesetzt, da sonst die Voraussetzunngen für ein Starten der Main-Methode und damit dem Programm, nicht gegeben wäre, da von außerhalb der Klasse darauf zugegriffen wird.
-	System.out.println("Hello World"); //Die Methode soll eine Nachricht ausgeben, "Hello World". println damit in eine neue Zeile geschrieben wird.
-    }
-}
-
-
