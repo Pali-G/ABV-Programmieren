01fd22c50b8680c5521eb5b38864a712e1cf5b9b
antivampir <gerbip00@zedat.fu-berlin.de>
2020-05-18 20:48:09 +0200

    Ein paar änderungen und Anfang simulation

--- Uebung4/Haus.java
+++ Uebung4/Haus.java
@@ -1,5 +1,5 @@
 package uebung4;
-import java.util.Scanner;
+import java.util.Random;
 
 
 public class Haus{
@@ -24,16 +24,8 @@ public class Haus{
     //Funktion zur eingabe von welche Wohnungen besetzt sind
     public static boolean[] defwohnungen(int Anzahlwohnungen){
 	boolean[] wng = new boolean[Anzahlwohnungen];
-	System.out.println("Geben sie true oder false ein um die Wohnunge zu besetzen");
 	for (int i = 0; i < Anzahlwohnungen; i++){
-	    System.out.println("Wohnung " + i);
-	    Scanner scanner = new Scanner(System.in);
-	    String input = scanner.nextLine();
-	    if (input.contains("t") == true){
-		wng[i] = true;
-	    }else if(input.contains("f") == true){
-		wng[i] = false;
-	    }
+	    wng[i] = new Random().nextBoolean();
 	}
 	return wng;
     }
@@ -46,6 +38,7 @@ public class Haus{
 		bewohnt++;
 	    }
 	}
+	
 	//System.out.println("Es sind " + bewohnt + " von " + Anzahlwohnungen + " bewohnt");
 	if (bewohnt == 0){
 	    leer = true;
@@ -53,12 +46,4 @@ public class Haus{
 	return leer;
     }
 
-    
-    /*public static boolean WreckingBall(int Baujahr, boolean[] wohnungen, int Anzahlwohnungen, boolean gebaut){
-    	int jahr = Year.now().getValue();
-    	if ((WohnungenLeer(wohnungen, Anzahlwohnungen) == true) && ((jahr - Baujahr) >= 60) && gebaut == true){
-    	    gebaut = false;
-    	}
-    	return gebaut;
-        }*/
 }
--- Uebung4/Strasse.java
+++ Uebung4/Strasse.java
@@ -1,32 +1,47 @@
-package uebung4;
-
-public class Strasse{
-
-	public static void main (String[] args){
-	Haus Haus1 = new Haus(5,1,1950,true);//Anazhlwohnungem, Hausnummer, Bauhjahr, Hausleer, gebaut
-	Haus.WohnungenLeer(Haus1.wohnungen, Haus1.Anzahlwohnungen);
-	Haus1.gebaut = WreckingBall(Haus1.Baujahr, Haus1.wohnungen, Haus1.Anzahlwohnungen, Haus1.gebaut, 2020);
+	package uebung4;
 	
-    }
-	int AnzahlGrundstücke;
+	import java.util.Random;
+	import java.util.Scanner;
 	
-	public static boolean WreckingBall(int Baujahr, boolean[] wohnungen, int Anzahlwohnungen, boolean gebaut, int jahr){
-		if ((Haus.WohnungenLeer(wohnungen, Anzahlwohnungen) == true) && ((jahr - Baujahr) >= 60) && gebaut == true){
-		    gebaut = false;
-		}
-		return gebaut;
-	}
-	/*public static void Grundstückeanlegen(AnzahlGrundstücke) {
-		for (int i = 0; i < AnzahlGrundstücke; i++) {
-			Hausname = "Haus" + i;
-			
-			Haus Hausname = new Haus()
-		}
-	}
-	public static void Simulation(int AGr,int Anfangsjahr, int Endjahr) {//AnzahlGrundstücke
+	public class Strasse{
+	
+		public static void main (String[] args){
 		
-		for (int jahr = Anfangsjahr; jahr <= Endjahr; jahr++) {
-			 
+	    }
+		int AnzahlGrundstücke;
+		
+		public static boolean WreckingBall(int Baujahr, boolean[] wohnungen, int Anzahlwohnungen, boolean gebaut, int jahr){
+			if ((Haus.WohnungenLeer(wohnungen, Anzahlwohnungen) == true) && ((jahr - Baujahr) >= 60) && gebaut == true){
+			    gebaut = false;
+			}
+			return gebaut;
 		}
-	}*/
-}
+		
+		public static Haus Hausbauen(Haus H, int aktuellesJahr) {
+			H.gebaut = true;
+			H.Baujahr = aktuellesJahr;
+			System.out.println("Bitte geben sie die Anzahl der Wohnungen ein");
+			Scanner scanner = new Scanner(System.in);
+		    String input = scanner.nextLine();
+		    H.Anzahlwohnungen = Integer.parseInt(input);
+	
+		    return H;
+		}
+		
+		//public static void 
+		public static void Simulation(int AGr,int Anfangsjahr, int Endjahr) {//AnzahlGrundstücke
+			Haus[] Häuser = new Haus[AGr];
+			for (int i = 0; i<AGr; i++) {
+				int Anzahlwohnungen = new Random().nextInt(10)+1;
+				int Hausnummer = i;
+				int Baujahr = new Random().nextInt(70)+1950;
+				boolean gebaut = new Random().nextBoolean();
+				boolean[] wng = new boolean[Anzahlwohnungen];
+				wng = Haus.defwohnungen(Anzahlwohnungen);
+			}
+			for (int jahr = Anfangsjahr; jahr <= Endjahr; jahr++) {
+				
+			}
+		}
+		
+	}
