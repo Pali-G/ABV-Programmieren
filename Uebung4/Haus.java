f209d584cc88534eb040ded69830fb2b3d4e1d08
antivampir <gerbip00@zedat.fu-berlin.de>
2020-05-19 13:11:13 +0200

    funktionierende Grundlegende Simulation

--- Uebung4/Haus.java
+++ Uebung4/Haus.java
@@ -1,49 +1,65 @@
 package uebung4;
+
 import java.util.Random;
 
+public class Haus {
 
-public class Haus{
-    
-    int Anzahlwohnungen;//static nur solange bis diese Variablen nicht mehr direkt von der mainfunction aufgerufen werden
-    int Hausnummer;//Grundstücknummer
-    int Baujahr;
-    boolean Hausbewohnt;// true ist bewohnt, false ist leer
-    boolean gebaut;//true ist gebaut
-    boolean[] wohnungen;//sh. Anzahlwohnungen
-    //evtl Strasse
+	int Anzahlwohnungen;// static nur solange bis diese Variablen nicht mehr direkt von der mainfunction
+						// aufgerufen werden
+	int Hausnummer;// Grundstücknummer
+	int Baujahr;
+	boolean Hausbewohnt;// true ist bewohnt, false ist leer
+	boolean gebaut;// true ist gebaut
+	boolean[] wohnungen;// sh. Anzahlwohnungen
 
-    //Konstruktor Klasse Haus
-    public Haus(int AW ,int HN, int BJ, boolean geb){
-	this.Anzahlwohnungen = AW;//So wird der Variable Hausnummer im Konstruktor ein wert zugeordnet
-	this.Hausnummer = HN;
-	this.Baujahr = BJ;
-	this.gebaut = geb;
-	this.wohnungen = defwohnungen(Anzahlwohnungen);
-    }
-
-    //Funktion zur eingabe von welche Wohnungen besetzt sind
-    public static boolean[] defwohnungen(int Anzahlwohnungen){
-	boolean[] wng = new boolean[Anzahlwohnungen];
-	for (int i = 0; i < Anzahlwohnungen; i++){
-	    wng[i] = new Random().nextBoolean();
+	// Konstruktor Klasse Haus
+	public Haus(int AW, int HN, int BJ, boolean geb) {
+		this.Anzahlwohnungen = AW;// So wird der Variable Hausnummer im Konstruktor ein wert zugeordnet
+		this.Hausnummer = HN;
+		this.Baujahr = BJ;
+		this.gebaut = geb;
+		this.wohnungen = defwohnungen(Anzahlwohnungen, gebaut);
 	}
-	return wng;
-    }
-    
-    public boolean WohnungenLeer(boolean[] wohnungen, int Anzahlwohnungen){//boolean[] wohnungen, int Anzahlwohnungen
-	int bewohnt = 0;
-	boolean leer = false;
-	for (int i = 0; i <= wohnungen.length -1; i++){
-	    if (wohnungen[i] == true){
-		bewohnt++;
-	    }
-	}
-	//System.out.println("Es sind " + bewohnt + " von " + Anzahlwohnungen + " bewohnt");
-	if (bewohnt == 0){
-	    leer = true;
-	}
-	return leer;
-    }
 
-    
+	// Funktion zur eingabe von welche Wohnungen besetzt sind
+	public static boolean[] defwohnungen(int Anzahlwohnungen, boolean gebaut) {
+		boolean[] wng = new boolean[Anzahlwohnungen];
+		if (gebaut == true) {
+		for (int i = 0; i < Anzahlwohnungen; i++) {
+			wng[i] = new Random().nextBoolean();
+		}
+		}else {
+			for (int i = 0; i < Anzahlwohnungen; i++) {
+				wng[i] = false;
+			}
+		}
+		return wng;
+	}
+
+	public boolean WohnungenLeer(boolean[] wohnungen, int Anzahlwohnungen) {// boolean[] wohnungen, int Anzahlwohnungen
+		int bewohnt = 0;
+		boolean leer = false;
+		for (int i = 0; i <= wohnungen.length - 1; i++) {
+			if (wohnungen[i] == true) {
+				bewohnt++;
+			}
+		}
+		// System.out.println("Es sind " + bewohnt + " von " + Anzahlwohnungen + "
+		// bewohnt");
+		if (bewohnt == 0) {
+			leer = true;
+		}
+		return leer;
+	}
+
+	public static Haus[] WreckingBall(int jahr, Haus[] H) {
+		for (int i = 0; i < H.length; i++) {
+			if ((H[i].WohnungenLeer(H[i].wohnungen, H[i].Anzahlwohnungen) == true) && ((jahr - H[i].Baujahr) >= 60)
+					&& H[i].gebaut == true) {
+				H[i].gebaut = false;
+			}
+		}
+		return H;
+	}
+
 }
--- Uebung4/Strasse.java
+++ Uebung4/Strasse.java
@@ -1,54 +1,167 @@
-	package uebung4;
-	
-	import java.util.Random;
-	import java.util.Scanner;
-	
-	public class Strasse{
-	
-		private static final Haus[] Haus = null;
+package uebung4;
 
-		public static void main (String[] args){
-			Simulation(7, 2000, 2100);
-		
-	    }
-		int AnzahlGrundstücke;
-		
-		public static Haus WreckingBall(int jahr, Haus H){
-			if ((H.WohnungenLeer(H.wohnungen, H.Anzahlwohnungen) == true) && ((jahr - H.Baujahr) >= 60) && H.gebaut == true){
-			    H.gebaut = false;
-			}
-			return H;
-		}
-		
-		public static Haus Hausbauen(Haus H, int aktuellesJahr) {
-			H.gebaut = true;
-			H.Baujahr = aktuellesJahr;
-			System.out.println("Bitte geben sie die Anzahl der Wohnungen ein");
-			Scanner scanner = new Scanner(System.in);
-		    int input = scanner.nextInt();
-		    H.Anzahlwohnungen = input;
-		    return H;
-		}
-		
+import java.util.Random;
 
-		
-		//public static void 
-		public static void Simulation(int AGr,int Anfangsjahr, int Endjahr) {//AnzahlGrundstücke
-			Haus[] Häuser = new Haus[AGr];
-			for (int i = 0; i<AGr; i++) {
-				int Anzahlwohnungen = new Random().nextInt(10)+1;
-				int Hausnummer = i;
-				int Baujahr = new Random().nextInt(70)+1950;
-				boolean gebaut = new Random().nextBoolean();
-				System.out.println(Anzahlwohnungen + Hausnummer + Baujahr);
-				System.out.println(gebaut);
-				Häuser[i] = new Haus(Anzahlwohnungen, Hausnummer, Baujahr, gebaut);
-			}
-			
-			for (int jahr = Anfangsjahr; jahr <= Endjahr; jahr++) {
-				
+public class Strasse {
+
+	public static void main(String[] args) throws InterruptedException {
+		Simulation(10, 2000, 2050);
+
+	}
+
+	static int Anzahlwohnungslose = 0;
+
+	public static Haus[] Wohnraumschaffen(Haus[] H, int Jahr, int benötigteWng) {
+		int i = 0;
+		int Hausnummer;
+		while (i < benötigteWng) {
+			Hausnummer = Grundstückfinden(H);
+			System.out.println(Hausnummer);
+			if (Hausnummer < 0) {
+				System.out.println("Es gibt zu wenig Grundstücke");
+				Anzahlwohnungslose += benötigteWng - i;
+				break;
+			} else {
+				System.out.println(H[Hausnummer].gebaut);
+				H[Hausnummer] = Hausbauen(H[Hausnummer], Jahr);
+				System.out.println(H[Hausnummer].gebaut);
+				i += H[Hausnummer].wohnungen.length;
 			}
 		}
+		return H;
+	}
 
-		
+
+	
+	public static int Grundstückfinden(Haus[] H) {
+		int HN = -1;
+		System.out.println(FreieGrundstücke(H));
+		for (int i = 0; i < H.length; i++) {
+			if (H[i].gebaut == false) {
+				HN = i;
+				break;
+			}
 		}
+		return HN;
+	}
+
+	public static boolean FreieGrundstücke(Haus[] H) {
+		boolean FrGr = false;
+		for (int i = 0; i < H.length; i++) {
+			if (H[i].gebaut == false) {
+				FrGr =true;
+			}
+		}
+		return FrGr;
+	}
+	
+	public static Haus Hausbauen(Haus H, int jahr) {
+		H.Baujahr = jahr;
+		H.gebaut = true;
+		H.Anzahlwohnungen = new Random().nextInt(20) + 1;
+		return H;
+	}
+
+	public static int[] Wohnungenbewohnt(Haus[] H) {
+		int[] Wohnungsstats = new int[2];
+		Wohnungsstats[0] = 0;// Anzahlwohnungenallgemein
+		Wohnungsstats[1] = 0;// belegteWohnungen
+		for (int i = 0; i < H.length; i++) {
+			for (int j = 0; j < H[i].Anzahlwohnungen; j++) {
+				if (H[i].gebaut == true) {
+					if (H[i].wohnungen[j] == true) {
+						Wohnungsstats[1]++;
+					}
+					Wohnungsstats[0]++;
+				}
+			}
+		}
+		return Wohnungsstats;
+	}
+
+	public static int[] MieterzumRausschmeißenfinden(Haus[] H) {
+		int[] Mieter = new int[2];
+		for (int k = 0; k < H.length; k++) {// Es wird durch die Haus liste iteriert
+			for (int j = 0; j < H[k].Anzahlwohnungen; j++) {// Es wird durch die Listes des kten Haus iteriert
+				if (H[k].wohnungen[j] == true) {
+					Mieter[0] = k;
+					Mieter[1] = j;
+					return Mieter;
+				}
+			}
+		}
+		return Mieter;
+	}
+
+	public static int[] Wohnungssuche(Haus[] H) {
+		int[] Mieter = new int[2];
+		for (int k = 0; k < H.length; k++) {// Es wird durch die Haus liste iteriert
+			for (int j = 0; j < H[k].Anzahlwohnungen; j++) {// Es wird durch die Listes des kten Haus iteriert
+				if (H[k].gebaut == true) {
+					if (H[k].wohnungen[j] == false) {
+						Mieter[0] = k;
+						Mieter[1] = j;
+						return Mieter;
+					}
+				}
+			}
+		}
+		return Mieter;
+	}
+
+	public static Haus[] umziehen(Haus[] H, int[] whnstats, int[] kug, int jahr) {
+		// Wegziehen
+		for (int i = 0; i < kug[1]; i++) {
+			int[] mieter = MieterzumRausschmeißenfinden(H);
+			H[mieter[0]].wohnungen[mieter[1]] = false;
+		}
+		System.out.println(FreieGrundstücke(H));
+		H = Haus.WreckingBall(jahr, H);
+		if (whnstats[0] - whnstats[1] < (kug[0] - kug[1]) && (kug[0] - kug[1]) > 0) {
+			System.out.println("Es gibt zuwenig Wohnungen, es müssen neue Häuser gebaut werden");
+			int benWhng = Math.abs(whnstats[0] - whnstats[1] - kug[0] - kug[1]);
+			System.out.println(benWhng);
+			H = Wohnraumschaffen(H, jahr, benWhng);
+		}
+		for (int i = 0; i < kug[0]; i++) {
+			int[] mieter = Wohnungssuche(H);
+			H[mieter[0]].wohnungen[mieter[1]] = true;
+		}
+		return H;
+	}
+
+	public static int[] Wohnungsmarkt(int[] wohnungsstats) {
+		int FreieWohnungen = wohnungsstats[0] - wohnungsstats[1];
+		int[] kommenundgehen = new int[2];
+		kommenundgehen[0] = new Random().nextInt(FreieWohnungen + 10) + (wohnungsstats[0] / 4);// Anzahl der
+																								// hinzuziehenden
+		kommenundgehen[1] = new Random().nextInt(wohnungsstats[1]);// Anzahl der wegziehenden
+		return kommenundgehen;
+	}
+
+	// public static void
+	public static void Simulation(int AGr, int Anfangsjahr, int Endjahr) throws InterruptedException {// AnzahlGrundstücke
+		Haus[] Häuser = new Haus[AGr];
+		for (int i = 0; i < AGr; i++) {
+			int Anzahlwohnungen = new Random().nextInt(20) + 1;
+			int Hausnummer = i;
+			int Baujahr = new Random().nextInt(70) + 1950;
+			boolean gebaut = new Random().nextBoolean();
+			Häuser[i] = new Haus(Anzahlwohnungen, Hausnummer, Baujahr, gebaut);
+		}
+		for (int jahr = Anfangsjahr; jahr <= Endjahr; jahr++) {
+			int[] whnstats = Wohnungenbewohnt(Häuser);
+			System.out.println("Es ist Das Jahr " + jahr);
+			System.out.println("Es sind " + whnstats[1] + " von " + whnstats[0] + " Wohnungen bewohnt");
+			int[] kommenundgehen = Wohnungsmarkt(whnstats);
+			Thread.sleep(1500);
+			System.out.println("Es ziehen " + kommenundgehen[0] + " Menschen hinzu und " + kommenundgehen[1] + " weg");
+			Häuser = umziehen(Häuser, whnstats, kommenundgehen, jahr);
+			if (Anzahlwohnungslose > 0) {
+				System.out.println("Es gibt " + Anzahlwohnungslose + " Wohnungslose in diese Strasse");
+			}
+			Thread.sleep(1500);
+		}
+	}
+
+}
